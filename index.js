a46841086426ef6ccddb506589ef93ca69f94364
Shikhar Gupta <53982388+SG115@users.noreply.github.com>
2022-12-21 12:37:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const emailForm = document.querySelector("#emailForm");
 
 const toast = document.querySelector(".toast");
 
-const baseURL = "https://insta--share.herokuapp.com";
+const baseURL = "https://insta-share-com.onrender.com";
 const uploadURL = `${baseURL}/api/files`;
 const emailURL = `${baseURL}/api/files/send`;
 
